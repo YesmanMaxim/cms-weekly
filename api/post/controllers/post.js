134b03e62e5b5790d6a24f6ef014ef7424774865
Yesman Maxim <m.esman@itransition.com>
2021-06-05 09:35:25 +0300

    Implement report generation

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,32 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async report(ctx) {
+    return stringify(await find(ctx), ctx.request.query.issue_no);
+  }
+};
 
-module.exports = {};
+async function find(ctx) {
+  let entities;
+  if (ctx.query._q) {
+    entities = await strapi.services.post.search(ctx.query);
+  } else {
+    entities = await strapi.services.post.find(ctx.query);
+  }
+  return entities;
+}
+
+function stringify(entities, issue) {
+  return [
+    `# **Tech Weekly. Issue #${issue}**`,
+    '\n\n',
+    entities.map(entity => stringifyEntity(entity)).join(''),
+  ].join('')
+}
+
+function stringifyEntity(entity) {
+  return `
+### [${entity.title}](${entity.short_url})
+> _${entity.description.trim()}_
+`;
+}
